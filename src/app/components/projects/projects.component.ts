105c09e63855a813771d1306119efe3d7af59e98
jesus <jesusdiaz013@gmail.com>
2024-02-25 13:25:54 -0300

    pc pass

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,14 +1,31 @@
 import { AfterViewInit, Component, ElementRef, HostListener } from '@angular/core';
+import { CarouselModule, CarouselResponsiveOptions } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
+import { ButtonModule } from 'primeng/button';
+import { ProjectDTO } from '../../models/projects/projectDTO';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule ,TagModule ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent  {
 
-  
+  projects : ProjectDTO [] = [
+    {"name" : "unNombre" , "img": "../../../assets/projects/1.jpg"},
+    {"name" : "unNombre" , "img": "../../../assets/projects/2.jpg"},
+    {"name" : "unNombre" , "img": "../../../assets/projects/3.jpg"},
+    {"name" : "unNombre" , "img": "../../../assets/projects/4.jpg"},
+    {"name" : "unNombre" , "img": "../../../assets/projects/5.jpg"},
+    {"name" : "unNombre" , "img": "../../../assets/projects/6.jpg"},
+    {"name" : "unNombre" , "img": "../../../assets/projects/7.jpg"},
+    {"name" : "unNombre" , "img": "../../../assets/projects/8.jpg"},
+  ];
+
+  responsiveOptions : CarouselResponsiveOptions [] = [];
+
+  interval : number = 3000;
 }
       
---
+++ src/app/models/projects/projectDTO.ts
@@ -0,0 +1,4 @@
+export interface ProjectDTO{
+    name : string,
+    img : string
+}
